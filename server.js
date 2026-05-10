d8c7feae594e65f455f4969287a3ab29860dde3a
Linath-Perera <linathperera17@gmail.com>
2026-05-10 16:36:41 +0530

    Server Setup and  database Connnect

---
+++ server.js
@@ -0,0 +1,20 @@
+const express =  require('express');
+const mongoose  = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Databbased  Connected"))
+.catch(err => console.error("Conection error:", err));
+
+app.get('/', (req,res) => {
+    res.send("Home4Paws API is Working: ");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT ,() => console.log(`Server on port ${PORT}`));
