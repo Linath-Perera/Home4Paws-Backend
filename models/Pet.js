2b4cd2100a22ff7dbac26672be609ac57806ac87
Linath-Perera <linathperera17@gmail.com>
2026-05-11 16:29:46 +0530

    Define pet model & schema

---
+++ models/Pet.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const PetSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    breed: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['Male','Female'],
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Available','Adopted'],
+        default: 'Available'
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {  
+        timestamps: true 
+    
+
+});
+module.exports = mongoose.model('Pet', PetSchema);
